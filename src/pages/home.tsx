8bd6d6be0715111770ef32f832f58f0e8090ae72
IsaacJReay <isaacjacksonreay@gmail.com>
2023-02-14 14:09:02 +0700

    fix responsiveness

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,26 +4,26 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
-      <div className="relative container flex justify-center px-10">
-        <div className="grid grid-rows-1 grid-flow-col gap-4 justify-center sm:py-10 md:pb-20 md:pt-12 lg:pb-24 lg:pt:16 xl:pb-30 xl:pt-20 ">
+      <center>
+        <div className="grid grid-rows-1 grid-flow-col gap-4 justify-center sm:py-10 md:pb-16 md:pt-12 lg:pb-18 lg:pt:16 xl:pb-20 xl:pt-20 ">
           <div className="row-span-1">
             <img
-              className="w-96 mt-12 flex items-center justify-center"
+              className="sm:h-[90px] md:h-[120px] xl:h-[150px] flex items-center justify-center"
               src="/images/koompi-cotent-server-logo.png"
             />
           </div>
         </div>
-      </div>
-      <Link to="/download">
-        <div className="relative container flex justify-center px-10">
-          <div className="grid grid-rows-1 grid-flow-col gap-4 justify-center sm:pt-24 md:pt-30 lg:pt-40 w-full">
+      </center>
+      <center>
+        <div className="grid grid-rows-1 grid-flow-col gap-4 justify-center sm:pt-24 md:pt-30 lg:pt-40 w-full">
+          <Link to="/download">
             <button id="start-button"
-              className="row-span-1 w-full bg-transparent hover:bg-blue-900 text-blue-900 font-semibold hover:text-white sm:py-4 sm:px-8 md:py-6 md:px-14 md:text-2xl border border-blue-900 hover:border-transparent rounded-lg">
+              className="row-span-1 w-full bg-transparent hover:bg-blue-900 text-blue-900 font-semibold hover:text-white sm:py-4 sm:px-8 md:py-6 md:px-14 md:text-2xl lg:py-8 lg:px-16 border border-blue-900 hover:border-transparent rounded-lg">
               START
             </button>
-          </div>
+          </Link>
         </div>
-      </Link>
+      </center>
       <div className="absolute bottom-0 left-0 sm:pb-6 sm:px-10 md:px-16 md:pb-10 ">
         <p className="text-slate-600 sm:text-xs md:text-base">
           KOOMPI © 2020 All Rights Reserved.
